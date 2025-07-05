4e7a74fd24b98b1ce606e33501a992070110a269
BhabishyaKC <bhabishyakc19@gmail.com>
2025-07-05 14:28:58 +0545

    Studied about data types

---
+++ chapter1/lesson5.js
@@ -0,0 +1,55 @@
+// DATA TYPES IN JAVASCRIPT
+
+
+// 1 . STRING
+
+let userName = "bhabishya" // anything within the double inverted quotes will give the type string
+console.log(userName)
+console.log(typeof userName) // typeof gives the type of the assigned variable value
+
+// 2. NUMBER
+
+let ageOfUser = 20
+console.log(ageOfUser)
+console.log(typeof ageOfUser) 
+
+
+// 3. BOOLEAN --> true/false---> yes/no
+
+let isProductCartAdded = false
+console.log(typeof isProductCartAdded)
+
+
+// 4. UNDEFINED
+
+let myName; // if we don't decalre any variable name then it gives undefined in console
+console.log(myName) // Output and type will be the same
+console.log(typeof myName)
+
+
+// 5. NULL
+ 
+let myBankBalance = null
+console.log(myBankBalance)
+console
+
+// 6. OBJECT  
+
+let myDetails = {myname:"Bhabishya KC", age: 23, address:'kathmmandu'}
+console.log(myDetails)
+console.log(typeof myDetails)
+
+
+// 7. ARRAY
+
+let listOfNum = [ 1,2 ,3 ,4,5 ,6]
+console.log(listOfNum)
+
+let objectArray = [{details:'i am bhabishya'},{age:29}]
+console.log(objectArray)
+
+
+
+
+
+
