333980babb6d63d9880163b5016d9ecd3e90c7b8
BhabishyaKC <bhabishyakc19@gmail.com>
2025-07-14 13:57:20 +0545

    learnd string and number addition and anout NaN

--- chapter1/lesson7.js
+++ chapter1/lesson7.js
@@ -32,4 +32,44 @@
  console.log(x**2)
  console.log(y**5)
 
+
+ // (+) operator means concatenation means it will just merge two variable no matter either string or a number 
+
+ const a = "12";
+ const b =3;
+
+  // Addition:
+ console.log(a+b);
+
+ // Subtraction 
+ console.log(a-b)
+
+ // Multiplication
+ console.log(a*b)
+
+
+ // Division 
+ console.log(a/b)
+
+ // Remainder
+
+ console.log(a%b)
+
+ // Exponentiation
+
+ console.log(a**2)
+ console.log(b**5)
+
+
+ // WHEN THE VARIABLE VALUE IS STRING THEN THE CONSOLE WILL RETUREN NaN --> Not a Number
+
+ const c = 'hello'
+ const d = 'world!'
  
+ console.log(c+d);
+ console.log(c-d)
+ console.log(c*d)
+ console.log(c/d)
+ console.log(c%d)
+ console.log(c**2)
+ console.log(d**2)
