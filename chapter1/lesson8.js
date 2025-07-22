edecfc47d0061e642caeb4105d9e6444b4c3d7b7
BhabishyaKC <bhabishyakc19@gmail.com>
2025-07-22 19:28:36 +0545

    learned about type conversion

---
+++ chapter1/lesson8.js
@@ -0,0 +1,38 @@
+// TYPE CONVERSION
+
+// CONVERTING STRING TO A NUMBER
+
+
+const a = '3'
+const b = '10'
+
+console.log(a+b) // addition operator always takes all the number as a strings
+
+
+const aNumber = Number(a)
+const bNumber= Number(b)
+
+console.log(aNumber + bNumber)
+
+
+/// EXAMPLE WITH TYPW CHECKING
+
+const c = Number(a)
+const d = Number(b)
+
+
+console.log (typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log( typeof d)
+
+
+const num = 123
+const str = String(num) // It will change the number into a string
+console.log(typeof str) 
+
+
+
+
+
+
